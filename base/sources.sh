bbb21baa978e2965f4236a0f09bb8ec6047b3213
Assxios <droge@student.42.fr>
2022-10-13 20:00:07 +0200

    Update sources.sh

--- base/sources.sh
+++ base/sources.sh
@@ -1,5 +1,4 @@
 echo "Generating sources files"
 
 mkdir -v $LFS/sources
-wget https://www.linuxfromscratch.org/lfs/view/stable/wget-list-sysv
 wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources
