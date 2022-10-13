9c2e716baad921be7c33520af2fadf3a82537563
Assxios <droge@student.42.fr>
2022-10-14 01:32:42 +0200

    Create Linuxapihdr.sh

---
+++ crosstool/Linuxapihdr.sh
@@ -0,0 +1,11 @@
+echo "Linux API headers"
+LFS=/mnt/lfs
+
+cd $LFS/sources
+tar -xvf linux-5.19.2.tar.xz
+cd linux-5.19.2
+
+make mrproper
+make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
