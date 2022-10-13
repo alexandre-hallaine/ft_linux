9406e2650308f6be4268ce136875b5bb89ac0699
Assxios <droge@student.42.fr>
2022-10-13 20:17:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sudo -iu root
-
 source base/fix_kali.sh
-source base/env.sh
 source base/sources.sh
 source base/folder.sh
 
-source user/create.sh
-source user/switch.sh
+#source user/create.sh
+#source user/switch.sh
