85a37773530dd0908259ecbfb51ce678febe8e37
Assxios <droge@student.42.fr>
2022-10-14 01:24:41 +0200

    Create binutils.sh

---
+++ crosstool/binutils.sh
@@ -0,0 +1,18 @@
+echo "Compiling the cross-toolchain"
+echo "Binutils pass 1"
+LFS=/mnt/lfs
+
+cd $LFS/sources
+tar -xvf binutils-2.39.tar.xz
+cd binutils-2.39/
+
+mkdir -v build
+cd build
+../configure --prefix=$LFS/tools \
+ --with-sysroot=$LFS \
+ --target=$LFS_TGT \
+ --disable-nls \
+ --enable-gprofng=no \
+ --disable-werror
+make
+make install
