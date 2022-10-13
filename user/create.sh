93cca89c3adaba83bb29ac0a3a5504aefe5245d2
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-10-13 19:36:23 +0200

    fix: lfs bash settings

--- user/create.sh
+++ user/create.sh
@@ -7,3 +7,5 @@ chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
+
+cp .bashrc .bash_profile /home/lfs/.
--- user/switch.sh
+++ user/switch.sh
@@ -2,8 +2,6 @@ echo "Switching to user lfs"
 
 sudo -iu lfs
 
-cp .bashrc .bash_profile ~
-
 source .bash_profile
 
 export MAKEFLAGS='-j4'
