441e04e2f6e12f32e872953d5c1b9ee76bd52b20
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-10-16 16:13:30 +0200

    fix: lfs cannot untar

--- user/create.sh
+++ user/create.sh
@@ -3,7 +3,7 @@ echo "Creating user"
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 
-chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
+chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools,sources}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
