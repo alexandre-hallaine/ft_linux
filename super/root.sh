d1b65e1d9c0c6d78825ae4567ae3267722f10d84
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-10-17 12:33:08 +0200

    fix: lfs env

--- super/root.sh
+++ super/root.sh
@@ -5,7 +5,7 @@ source base/fix_kali.sh
 LFS=/mnt/lfs
 
 source base/partitions.sh
-bash base/folder.sh >/dev/null
-bash base/sources.sh >/dev/null
+source base/folder.sh >/dev/null
+source base/sources.sh >/dev/null
 
-bash user/create.sh >/dev/null
+source user/create.sh >/dev/null
