a1990504979598c96b6212c1ea5ab9408be924e9
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-10-17 12:38:12 +0200

    add: m4

---
+++ compiling/temptools/m4.sh
@@ -0,0 +1,10 @@
+echo "M4"
+tar -xvf m4-1.4.19.tar.xz
+cd m4-1.4.19
+
+./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
+
+make
+make DESTDIR=$LFS install
--- super/lfs.sh
+++ super/lfs.sh
@@ -5,9 +5,12 @@ source user/setting.sh
 GIT_FOLDER=`pwd`
 cd $LFS/sources
 
-echo >&2 "Compiling the cross-toolchain"
-bash $GIT_FOLDER/crosstools/binutils.sh >/dev/null
-bash $GIT_FOLDER/crosstools/gcc.sh >/dev/null
-bash $GIT_FOLDER/crosstools/linuxapihdr.sh >/dev/null
-bash $GIT_FOLDER/crosstools/glibc.sh >/dev/null
-bash $GIT_FOLDER/crosstools/libstdc++.sh >/dev/null
+echo >&2 "Compiling a Cross-Toolchain"
+bash $GIT_FOLDER/compiling/crosstools/binutils.sh >/dev/null
+bash $GIT_FOLDER/compiling/crosstools/gcc.sh >/dev/null
+bash $GIT_FOLDER/compiling/crosstools/linuxapihdr.sh >/dev/null
+bash $GIT_FOLDER/compiling/crosstools/glibc.sh >/dev/null
+bash $GIT_FOLDER/compiling/crosstools/libstdc++.sh >/dev/null
+
+echo >&2 "Cross Compiling Temporary Tools"
+bash $GIT_FOLDER/compiling/temptools/m4.sh >/dev/null
