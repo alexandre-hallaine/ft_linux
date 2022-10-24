5e35b3afbb6868be1c8db5efd7f924353d5d7fdd
Assxios <droge@student.42.fr>
2022-10-24 22:40:18 +0200

    Update: forgot to launch all the new scripts

--- scripts/build/system.sh
+++ scripts/build/system.sh
@@ -10,3 +10,10 @@ bash /system/bzip2.sh >/dev/null
 bash /system/xz.sh >/dev/null
 bash /system/zstd.sh >/dev/null
 bash /system/file.sh >/dev/null
+bash /system/readline.sh >/dev/null
+bash /system/m4.sh >/dev/null
+bash /system/bc.sh >/dev/null
+bash /system/flex.sh >/dev/null
+bash /system/tcl.sh >/dev/null
+bash /system/expect.sh >/dev/null
+bash /system/dejagnu.sh >/dev/null
