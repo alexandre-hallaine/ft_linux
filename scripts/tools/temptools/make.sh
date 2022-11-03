113eac1e5a27d6b871daaf831e2339efd770af97
Assxios <53396610+Assxios@users.noreply.github.com>
2022-11-03 20:38:02 +0100

    Fix: change ver in script aswell

--- scripts/tools/temptools/make.sh
+++ scripts/tools/temptools/make.sh
@@ -1,7 +1,7 @@
 echo >&2 "Make"
-rm -rf make-4.4
-tar -xvf make-4.4.tar.gz
-cd make-4.4
+rm -rf make-4.3
+tar -xvf make-4.3.tar.gz
+cd make-4.3
 
 ./configure --prefix=/usr   \
             --without-guile \
